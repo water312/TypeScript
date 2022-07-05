82cee49e57196d087a4f6a5487bea7dbf740a6bd
성수 <whtjdtn312@gmail.com>
2022-07-05 11:42:55 +0900

    jsdocs

---
+++ jsdoc/src/index.js
@@ -0,0 +1,39 @@
+/**
+ * 내부 구현이 되어있지 않다 => 즉 선언부를 통해 설계만 했다.
+ */
+/** Class representing a point. */
+class Point {
+  /**
+   * Create a point.
+   * @param {number} x - The x value.
+   * @param {number} y - The y value.
+   */
+  constructor(x, y) {
+      // ...
+  }
+
+  /**
+   * Get the x value.
+   * @return {number} The x value.
+   */
+  getX() {
+      // ...
+  }
+
+  /**
+   * Get the y value.
+   * @return {number} The y value.
+   */
+  getY() {
+      // ...
+  }
+
+  /**
+   * Convert a string containing two comma-separated numbers into a point.
+   * @param {string} str - The string containing two comma-separated numbers.
+   * @return {Point} A Point object.
+   */
+  static fromString(str) {
+      // ...
+  }
+}
